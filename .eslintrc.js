8315077082b5c50fdd579cac91bc18cf7a663b4b
Alessandro Curcu <aleqrq@gmail.com>
2018-06-26 12:02:13 +0200

    Primo commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": [
+        "airbnb",
+        "prettier",
+        "prettier/react"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2016,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "jest": true,
+        "es6": true
+    },
+    "rules": {
+        "strict": 0,
+        "no-console": "off",
+        "no-unused-vars": "warn",
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+        "prefer-numeric-literals": 0,
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/prefer-stateless-function": 0,
+        "prettier/prettier": [
+            "error",
+            {
+                "trailingComma": "es5",
+                "singleQuote": true
+            }
+        ]    
+    },
+    "plugins": [
+        "prettier"
+    ]
+};
---
+++ src/Components/Prova.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Prova extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Questa è una prova</h1>
+      </div>
+    );
+  }
+}
+
+export default Prova;
