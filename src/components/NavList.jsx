fc29c92b7d319a4fe3cc8bfa8f9d05e2b62be24a
lauramarinab <>
2018-06-27 12:35:20 +0200

    modifiche alla componente sidebar

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class NavList extends Component {
   render() {
     return (
       <nav>
-        <ul>
+        <ul className="navigation">
           <li>
-            <h3>Homepage</h3>
+            <NavLink activeClassName="isActive" exact to="/">
+              <h3>Homepage</h3>
+            </NavLink>
           </li>
           <li>
             <h3>Account</h3>
             <ul>
-              <li>Trend</li>
-              <li>Network</li>
+              <li>
+                <NavLink activeClassName="isActive" exact to="/account/trend">
+                  Trend
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="isActive" exact to="/account/network">
+                  Network
+                </NavLink>
+              </li>
             </ul>
           </li>
           <li>
             <h3>Hashtag</h3>
             <ul>
-              <li>Trend</li>
-              <li>Network</li>
+              <li>
+                <NavLink activeClassName="isActive" exact to="/hashtag/trend">
+                  Trend
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="isActive" exact to="/hashtag/network">
+                  Network
+                </NavLink>
+              </li>
             </ul>
           </li>
         </ul>
