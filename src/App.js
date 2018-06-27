8da42d62113742b74b941ea8d5d3985f02ae1778
FrancescaCitter <fra.citter@gmail.com>
2018-06-27 16:39:16 +0200

    Varie franpa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './css/App.css';
 import Logo from './components/Logo';
 import NavList from './components/NavList';
 import Chart from './components/grafici/Chart';
+// import { Route, Switch } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Logo />
-        <p className="Officina__Sans">Prova Officina Sans</p>
-        <p className="Officina__Serif">Prova Officina Serif</p>
+        {/* <p className="Officina__Sans">Prova Officina Sans</p>
+        <p className="Officina__Serif">Prova Officina Serif</p> */}
         <NavList />
         <Chart />
+        <Sidebar />
       </div>
     );
   }
