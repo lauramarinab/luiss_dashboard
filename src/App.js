1eb93231746fd7d440a45e1babb4a39ef3005927
FrancescaCitter <fra.citter@gmail.com>
2018-06-27 15:16:54 +0200

    creata modale

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './css/App.css';
 import Logo from './components/Logo';
 import NavList from './components/NavList';
+import Chart from './components/grafici/Chart';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <p className="Officina__Sans">Prova Officina Sans</p>
         <p className="Officina__Serif">Prova Officina Serif</p>
         <NavList />
+        <Chart />
       </div>
     );
   }
