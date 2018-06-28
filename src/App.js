670f9dd53d7ca30b6224e3475b82acfe77c8565b
Alessandro Curcu <aleqrq@gmail.com>
2018-06-28 09:38:23 +0200

    Risolve merge

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React, { Component } from 'react';
 import './css/App.css';
 // import { Route, Switch } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
-<<<<<<< HEAD
-import Chart from './components/grafici/Chart'
-=======
 import Chart from './components/grafici/Chart';
 import data from './json/luissDays.json';
->>>>>>> 2a20252d2c76c0014170b827270bfe458e834d9c
 
 class App extends Component {
   componentDidMount() {
@@ -18,11 +14,7 @@ class App extends Component {
     return (
       <div>
         <Sidebar />
-<<<<<<< HEAD
-        <Chart />
-=======
         <Chart>ciao</Chart>
->>>>>>> 2a20252d2c76c0014170b827270bfe458e834d9c
       </div>
     );
   }
