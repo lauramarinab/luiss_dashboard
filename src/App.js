2201ab21531ac06a3299208c8667d9dbc51c2015
EyefarTalot <32478757+EyefarTalot@users.noreply.github.com>
2018-06-30 17:13:05 +0200

    componente NetworkCharts.jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './css/App.css';
 // import { Route, Switch } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
-import Chart from './components/grafici/Chart'
+import Chart from './components/grafici/Chart';
+import NetworkCharts from './components/grafici/NetworkCharts';
 
 class App extends Component {
   render() {
