1b57945e014f102cffc30131d96dd5adfc742169
lauramarinab <>
2018-07-03 17:57:40 +0200

    Aggiunge doubleBarChart

--- src/components/account/TrendAccount.jsx
+++ src/components/account/TrendAccount.jsx
@@ -27,6 +27,9 @@ class TrendAccount extends Component {
     luissInvolvement: [],
     luissInvolvementBar: [],
     entitiesActivityInvolvement: [],
+    peopleActivity: [],
+    peopleInvolvement: [],
+    peopleActivityInvolvement: [],
   };
 
   componentDidMount() {
@@ -39,6 +42,8 @@ class TrendAccount extends Component {
         luissActivity: res[0].activity,
         luissInvolvement: res[0].involvement,
         luissInvolvementBar: luissInvolvementBarFormattedData,
+        peopleActivity: res[1].activity,
+        peopleInvolvement: res[1].involvement,
         // entitiesActivityInvolvement: [],
       });
 
@@ -70,20 +75,31 @@ class TrendAccount extends Component {
 
   getEntities = dataArr => dataArr.map(el => el.entity);
 
-  getAllLuissEntities = () => {
-    const luissEntitiesByActivity = this.getEntities(this.state.luissActivity);
-    const luissEntitiesByInvolvement = this.getEntities(
-      this.state.luissInvolvement
-    );
-    const luissEntitiesArr = luissEntitiesByActivity.concat(
-      luissEntitiesByInvolvement
-    );
+  // getAllLuissEntities = () => {
+  //   const luissEntitiesByActivity = this.getEntities(this.state.luissActivity);
+  //   const luissEntitiesByInvolvement = this.getEntities(
+  //     this.state.luissInvolvement
+  //   );
+  //   const luissEntitiesArr = luissEntitiesByActivity.concat(
+  //     luissEntitiesByInvolvement
+  //   );
+
+  //   // I set eliminano i doppioni nell'array
+  //   const luissEntitiesSet = new Set(luissEntitiesArr);
+  //   const luissEntities = Array.from(luissEntitiesSet);
+
+  //   return luissEntities;
+  // };
+  getAllEntities = (typeActivity, typeInvolvement) => {
+    const EntitiesByActivity = this.getEntities(typeActivity);
+    const EntitiesByInvolvement = this.getEntities(typeInvolvement);
+    const EntitiesArr = EntitiesByActivity.concat(EntitiesByInvolvement);
 
     // I set eliminano i doppioni nell'array
-    const luissEntitiesSet = new Set(luissEntitiesArr);
-    const luissEntities = Array.from(luissEntitiesSet);
+    const EntitiesSet = new Set(EntitiesArr);
+    const Entities = Array.from(EntitiesSet);
 
-    return luissEntities;
+    return Entities;
   };
 
   getSingleEntityData = (ent, setOfData) => {
@@ -166,6 +182,58 @@ class TrendAccount extends Component {
     );
   };
 
+  formatDataForDoubleBarChart = () => {
+    const peopleActivity = this.state.peopleActivity;
+    const peopleInvolvement = this.state.peopleInvolvement;
+
+    const peopleActivityFormatted = peopleActivity.map(el =>
+      this.changeProp('frequency', 'Attività', el)
+    );
+
+    const peopleInvolvementFormatted = peopleInvolvement.map(el =>
+      this.changeProp('frequency', 'Coinvolgimento', el)
+    );
+
+    const entities = this.getAllEntities(
+      this.state.peopleActivity,
+      this.state.peopleInvolvement
+    );
+
+    const createPeopleAccount = entità => {
+      const entitàAttività = peopleActivityFormatted.find(
+        el => el.entity === entità
+      );
+      const entitàCoinvolgimento = peopleInvolvementFormatted.find(
+        el => el.entity === entità
+      );
+
+      if (!entitàAttività) {
+        return {
+          Attività: 0,
+          Coinvolgimento: this.formatDecimalData(
+            entitàCoinvolgimento.Coinvolgimento
+          ),
+          entity: entità,
+        };
+      } else if (!entitàCoinvolgimento) {
+        return {
+          Attività: entitàAttività.Attività,
+          Coinvolgimento: 0,
+          entity: entità,
+        };
+      }
+      return {
+        Attività: entitàAttività.Attività,
+        Coinvolgimento: this.formatDecimalData(
+          entitàCoinvolgimento.Coinvolgimento
+        ),
+        entity: entità,
+      };
+    };
+    const peopleForDoubleBarChart = entities.map(createPeopleAccount);
+    return peopleForDoubleBarChart;
+  };
+
   getAllTrendAccountDataByDates = (startDate, endDate, selectedOption) => {
     Api.getAllTrendAccountData(startDate, endDate).then(res => {
       this.setState({
@@ -193,7 +261,25 @@ class TrendAccount extends Component {
     );
   };
 
-  showProva = () => { };
+  updateDoubleBarchart = (startDate, endDate) => {
+    Api.getAllTrendAccountData(startDate, endDate).then(res => {
+      this.setState({
+        peopleActivity: res[1].activity,
+        peopleInvolvement: res[1].involvement,
+      });
+      this.formatDataForDoubleBarChart();
+    });
+  };
+
+  showProva = () => {
+    // console.log(
+    //   this.getAllEntities(
+    //     this.state.peopleActivity,
+    //     this.state.peopleInvolvement
+    //   )
+    // );
+    this.formatDataForDoubleBarChart();
+  };
 
   render() {
     return (
@@ -204,7 +290,10 @@ class TrendAccount extends Component {
             <Chart
               chartTitle="Quanto sono attivi gli account Luiss?"
               doesSelectExist
-              selectOptions={this.getAllLuissEntities()}
+              selectOptions={this.getAllEntities(
+                this.state.luissActivity,
+                this.state.luissInvolvement
+              )}
               formatDataForLineChart={this.updateChartByEntity}
               graphExplanation={graphExplanation[0]}
               getActivityInvolvementDates={this.getAllTrendAccountDataByDates}
@@ -265,13 +354,13 @@ class TrendAccount extends Component {
                   <YAxis style={{ fontSize: 12, fontWeight: 100 }} />
                   <Tooltip />
                   <Bar dataKey="Coinvolgimento" fill="#a6ba66">
-                    <LabelList
+                    {/* <LabelList
                       dataKey="entity"
                       position="insideBottomLeft"
                       angle={-90}
                       style={{ fontSize: 12, fontWeight: 100 }}
                       offset={17}
-                    />
+                    /> */}
                   </Bar>
                 </BarChart>
               </ResponsiveContainer>
@@ -279,33 +368,42 @@ class TrendAccount extends Component {
             <Chart
               chartTitle="Chi è più attivo e coinvolgente nel mondo Luiss?"
               doesSelectExist={false}
+              graphExplanation={graphExplanation[2]}
+              getActivityInvolvementDates={this.updateDoubleBarchart}
             >
               <ResponsiveContainer width="95%" aspect={4.0 / 3.0}>
                 <BarChart
                   width={730}
                   height={250}
-                  data={[]}
+                  data={this.formatDataForDoubleBarChart()}
+                  layout="vertical"
                   margin={{ top: 40, right: 20, left: 10, bottom: 20 }}
                 >
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis
+                    type="number"
+                    style={{ fontSize: 12, fontWeight: 100 }}
+                  />
+                  <YAxis
                     dataKey="entity"
+                    type="category"
                     minTickGap={-300}
                     textAnchor="end"
                     angle={-40}
                     style={{ fontSize: 0, fontWeight: 100 }}
                   />
-                  <YAxis style={{ fontSize: 12, fontWeight: 100 }} />
                   <Tooltip />
-                  <Bar dataKey="frequency" fill="#a6ba66">
-                    <LabelList
+                  <Legend />
+                  <Bar dataKey="Attività" fill="#ca4f24">
+                    {/* <LabelList
                       dataKey="entity"
-                      position="insideBottomLeft"
-                      angle={-90}
-                      style={{ fontSize: 12, fontWeight: 100 }}
-                      offset={17}
-                    />
+                      position="left"
+                      angle={0}
+                      style={{ fontSize: 16, fontWeight: 700 }}
+                      offset={0}
+                    /> */}
                   </Bar>
+                  <Bar dataKey="Coinvolgimento" fill="#6d7eb0" />
                 </BarChart>
               </ResponsiveContainer>
             </Chart>
